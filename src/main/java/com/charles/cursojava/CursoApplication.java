005c9fef8194537b9536aa3b565e4e524a8c565b
Charles <charlescostas@gmail.com>
2023-03-04 16:37:44 -0300

    page

--- src/main/java/com/charles/cursojava/CursoApplication.java
+++ src/main/java/com/charles/cursojava/CursoApplication.java
@@ -59,8 +59,16 @@ public class CursoApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception{
 		
+		Categoria cat0 = new Categoria(null,"Escritorio");
 		Categoria cat1 = new Categoria(null,"Informatica");
-		Categoria cat2 = new Categoria(null,"Escritorio");
+		Categoria cat2 = new Categoria(null,"Cama");
+		Categoria cat3 = new Categoria(null,"mouse");
+		Categoria cat4 = new Categoria(null,"aaaaaa");
+		Categoria cat5 = new Categoria(null,"bbbb");
+		Categoria cat6 = new Categoria(null,"ccccc");
+		Categoria cat7 = new Categoria(null,"ddd");
+		Categoria cat8 = new Categoria(null,"wwwwwww");
+		Categoria cat9 = new Categoria(null,"pppppp");
 		
 		Produto p1 = new Produto(null,"Computador", 2000.00);
 		Produto p2 = new Produto(null,"Impressora", 800.00);
@@ -73,7 +81,7 @@ public class CursoApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat0,cat1,cat2,cat3,cat4,cat5,cat6,cat7,cat8,cat9));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 
 		Estado est1 = new Estado(null,"Minas Gerais");
