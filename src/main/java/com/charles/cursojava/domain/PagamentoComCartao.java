435bdef49fa22df8a8e56680fe7c63b9fde706ca
Charles <charlescostas@gmail.com>
2023-03-14 09:48:52 -0300

    insert pedido

--- src/main/java/com/charles/cursojava/domain/PagamentoComCartao.java
+++ src/main/java/com/charles/cursojava/domain/PagamentoComCartao.java
@@ -1,10 +1,12 @@
 package com.charles.cursojava.domain;
 
 import com.charles.cursojava.domain.enums.EstadoPagamento;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 
 import jakarta.persistence.Entity;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento{
 	private static final long serialVersionUID = 1L;
 	
