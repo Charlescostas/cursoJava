fe463ab6353f63c55725fde54966052cbda6e64d
Charles <charlescostas@gmail.com>
2023-03-17 09:21:27 -0300

    tostring pedido

--- src/main/java/com/charles/cursojava/services/PedidoService.java
+++ src/main/java/com/charles/cursojava/services/PedidoService.java
@@ -13,6 +13,7 @@ import com.charles.cursojava.domain.enums.EstadoPagamento;
 import com.charles.cursojava.repositories.ItemPedidoRepository;
 import com.charles.cursojava.repositories.PagamentoRepository;
 import com.charles.cursojava.repositories.PedidoRepository;
+import com.charles.cursojava.repositories.ProdutoRepository;
 import com.charles.cursojava.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -33,6 +34,9 @@ public class PedidoService {
 	@Autowired
 	private ProdutoService produtoService;
 	
+	@Autowired
+	private ClienteService clienteService;
+
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
@@ -42,6 +46,7 @@ public class PedidoService {
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if (obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -52,10 +57,12 @@ public class PedidoService {
 		pagamentoRepository.save(obj.getPagamento());
 		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
 			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
+		System.out.println(obj);
 		return obj;
 	}
 }
