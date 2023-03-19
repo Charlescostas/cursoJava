cfd1c2be1a52cf524f8a6727d71f9af7c4adf14a
Charles <charlescostas@gmail.com>
2023-03-19 17:29:43 -0300

    html email

--- src/main/java/com/charles/cursojava/config/DevConfig.java
+++ src/main/java/com/charles/cursojava/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.charles.cursojava.services.DBService;
+import com.charles.cursojava.services.EmailService;
+import com.charles.cursojava.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -27,6 +29,11 @@ public class DevConfig {
 		}
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
 
