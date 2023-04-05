3e5a24c3db355d6254c87ad0ca6c6dc2b1257117
Charles <charlescostas@gmail.com>
2023-04-05 10:45:50 -0300

    cliente so recupera seus pedidos

--- src/main/java/com/charles/cursojava/services/PedidoService.java
+++ src/main/java/com/charles/cursojava/services/PedidoService.java
@@ -4,8 +4,12 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.charles.cursojava.domain.Cliente;
 import com.charles.cursojava.domain.ItemPedido;
 import com.charles.cursojava.domain.PagamentoComBoleto;
 import com.charles.cursojava.domain.Pedido;
@@ -13,6 +17,8 @@ import com.charles.cursojava.domain.enums.EstadoPagamento;
 import com.charles.cursojava.repositories.ItemPedidoRepository;
 import com.charles.cursojava.repositories.PagamentoRepository;
 import com.charles.cursojava.repositories.PedidoRepository;
+import com.charles.cursojava.security.UserSS;
+import com.charles.cursojava.services.exceptions.AuthorizationException;
 import com.charles.cursojava.services.exceptions.ObjectNotFoundException;
 
 import jakarta.transaction.Transactional;
@@ -69,5 +75,15 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 }
